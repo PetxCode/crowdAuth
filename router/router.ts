c6c09b1abae6c712ab0a0b8471416ea5d83bb8c2
Destiny onuiri <destinyonuiri@gmail.com>
2023-09-28 17:35:01 +0100

    start

--- router/router.ts
+++ router/router.ts
@@ -14,23 +14,23 @@ import {
 const router = express.Router();
 
 router.route("/create-account").post(createAccount);
+
+router.route("/sign-in-account").post(SignInUser);
+
+router.route("/:token/verify-account").get(firstAccountVerification);
+
+router.route("/:token/verify-account").get(accountVerification);
+
 router.route("/all-account").get(allAccount);
 
 router.route("/:accountID/single-account").get(getSingleAccount);
-(firstAccountVerification);
-
-router.route("/:token/verify-account").get(accountVerification);
-router.route("/sign-in-account").post(SignInUser);
-
 
 router.route("/:userID/delete").delete(deleteUser);
 
 router.route("/reset-account-password").patch(resetAccountPassword);
 
-
 router.route("/:token/change-account-password").patch(changeAccountPassword);
 
-router.route("/:token/verify-account").get(accountVerification);
 
 
 
